c8360d812ecd579174661796cf9af40036334c4c
wangxi <wangxi8206@sina.com>
2022-01-04 18:13:33 +0800

    init

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,47 +1,30 @@
-import { BigInt } from "@graphprotocol/graph-ts"
 import { Eth2Deposit, DepositEvent } from "../generated/Eth2Deposit/Eth2Deposit"
-import { ExampleEntity } from "../generated/schema"
+import { Deposit } from "../generated/schema"
 
 export function handleDepositEvent(event: DepositEvent): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+  let entity = Deposit.load(event.transaction.hash.toHex())
 
   // Entities only exist after they have been saved to the store;
   // `null` checks allow to create entities on demand
   if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+    entity = new Deposit(event.transaction.hash.toHex())
+    entity.from = event.transaction.from;
+    entity.value = event.transaction.value;
+    entity.timestamp = event.block.timestamp;
+    entity.tx = event.transaction.hash;
+    entity.count = 0;
+  }else {
+    const count = entity.count + 1;
+    entity = new Deposit(event.transaction.hash.toHex() + count.toString())
+    entity.from = event.transaction.from;
+    entity.value = event.transaction.value;
+    entity.timestamp = event.block.timestamp;
+    entity.tx = event.transaction.hash;
+    entity.count = count;
   }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.pubkey = event.params.pubkey
-  entity.withdrawal_credentials = event.params.withdrawal_credentials
 
   // Entities can be written to the store with `.save()`
   entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.get_deposit_count(...)
-  // - contract.get_deposit_root(...)
-  // - contract.supportsInterface(...)
 }
